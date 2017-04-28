d9438c1ce386f89d6d4910816b35c53c8b7cfc18
ur5fot <dima.toropov.lardan@gmail.com>
2017-04-28 10:58:52 +0300

    fist

---
+++ domains/prestashop171/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '192.168.2.99',
+    'database_port' => '',
+    'database_name' => 'prestashop171',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'sR17HF7cjyq2Um8JX6rh1UjSDKmudZSDnatkJBuusSmf0iMvLy3EPVgB',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-04-26',
+    'locale' => 'ru-RU',
+    'cookie_key' => 'qiWPyovuT5ScNQGInSbdrGQj36AKHG01eF7Es3bkhFqIjfx16xsacey2',
+    'cookie_iv' => 'Za8QpY2Y',
+    'new_cookie_key' => 'def00000b9983d93e0593c3083676881477134503b83e600955227696269a7c67fa30fbbc38f4244ed4521a9e976160ceac2d93f3bd295a666f928bf32518d085f432b02',
+  ),
+);
---
+++ domains/prestashop171delolt/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '192.168.2.99',
+    'database_port' => '',
+    'database_name' => 'prestashop171_defolt',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'o7aikp3f8HkVA6nbucfUi7rS5ce0YOAUOSQg8nDW3h3Ond5AhTrwx1Qp',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-04-14',
+    'locale' => 'ru-RU',
+    'cookie_key' => 'QPHkwkNLyR8azH6RTkgjgjzhKD53S6zkw7RKPvmGHtYnOSpT2H2JxLei',
+    'cookie_iv' => 'JnVYyxZQ',
+    'new_cookie_key' => 'def00000384b710705c8135667faa38b9ec76f8bd2d24b0eeeeb63ce95acf537787aa42ba27b0b27034ef2dd4477112a99a167b9126bcb9c85f77b286715489e2fc1ef45',
+  ),
+);
